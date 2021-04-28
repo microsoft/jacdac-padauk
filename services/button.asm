1b00f409093c225da6abb1e29c898a6377511f9d
Michal Moskal <michal@moskal.me>
2021-04-28 16:52:35 -0700

    pressed->pressure in button service

--- services/button.asm
+++ services/button.asm
@@ -1,9 +1,13 @@
 #define SERVICE_CLASS 0x1473a263
-#define SENSOR_SIZE 1
+#define SENSOR_SIZE 2
 
 #define JD_BUTTON_EV_DOWN 0x01
 #define JD_BUTTON_EV_UP 0x02
 #define JD_BUTTON_EV_HOLD 0x81
+
+#define JD_LED_REG_RO_ANALOG 0x80
+
+txp_analog_button equ 6
 
 	BYTE	t_sample
 	BYTE    t_btn_hold
@@ -24,6 +28,14 @@ ENDM
 ENDM
 
 .serv_prep_tx MACRO
+	if (tx_pending.txp_analog_button) {
+		set0 tx_pending.txp_analog_button
+		clear pkt_payload[0]
+		.mova pkt_size, 1
+		.mova pkt_service_command_l, JD_LED_REG_RO_ANALOG
+		ret
+	}
+
 	ifset tx_pending.txp_event
 		goto ev_prep_tx
 	.sensor_prep_tx
@@ -38,6 +50,8 @@ button_inactive:
 	ifset ZF // state==0
 		goto loop // just keep going
 	clear sensor_state[0]
+	clear sensor_state[1]
+
 		// snapshot duration
 		mov a, t16_1ms
 		sub a, btn_down_l
@@ -58,7 +72,8 @@ button_hold:
 	mov a, JD_BUTTON_EV_HOLD
 	goto ev_send
 button_down:
-	.mova sensor_state[0], 1
+	.mova sensor_state[0], 0xff
+	.mova sensor_state[1], 0xff
 	.disint
 		.mova btn_down_l, t16_1ms
 		.mova btn_down_h, t16_262ms
@@ -68,6 +83,13 @@ button_down:
 	goto ev_send
 
 serv_rx:
+	mov a, pkt_service_command_h
+	if (a == JD_HIGH_REG_RO_GET) {
+		mov a, pkt_service_command_l
+		if (a == JD_LED_REG_RO_ANALOG) {
+			set1 tx_pending.txp_analog_button
+		}
+	}
 	.sensor_rx
 
 .serv_ev_payload EXPAND
