78c8252530fe509a32ee8988f36e83c051ab862b
Michal Moskal <michal@moskal.me>
2021-06-03 15:22:54 -0700

    various fixes

--- services/power.asm
+++ services/power.asm
@@ -4,7 +4,6 @@
 #define JD_POWER_POWER_STATUS_POWERING 0x1
 #define JD_POWER_POWER_STATUS_OVERLOAD 0x2
 #define JD_POWER_POWER_STATUS_OVERPROVISION 0x3
-#define JD_POWER_POWER_STATUS_STARTUP 0x4
 
 #define JD_POWER_REG_RW_ALLOWED JD_REG_RW_INTENSITY
 #define JD_POWER_REG_RO_POWER_STATUS 0x81
@@ -15,6 +14,7 @@
 	BYTE	t_re_enable
 	BYTE    pwr_status
 	BYTE    prev_pwr_status
+	BYTE    switch_cnt
 
 txp_pwr_shutdown equ txp_serv0
 txp_pwr_allowed equ txp_serv1
@@ -37,7 +37,7 @@ ENDM
 	call rng_next
 	and a, 0xf // 0-240ms
 	mov t_next_shutdown, a
-	.mova pwr_status, JD_POWER_POWER_STATUS_STARTUP
+	pwr_status = JD_POWER_POWER_STATUS_POWERING
 ENDM
 
 .serv_process EXPAND
@@ -68,10 +68,6 @@ ENDM
 	mov a, pwr_status
 	if (a != prev_pwr_status) {
 		mov prev_pwr_status, a
-		if (a == JD_POWER_POWER_STATUS_POWERING) {
-			// if we switched to Powering, shedule shutdown to be sent ASAP
-			.mova t_next_shutdown, t16_16ms
-		}
 		goto ev_send
 	}
 	.t16_chk t16_16ms, t_next_shutdown, <goto send_shutdown>
@@ -86,11 +82,16 @@ send_shutdown:
 	add a, 24
 	.t16_set_a t16_16ms, t_next_shutdown
 	mov a, pwr_status
-	if (a == JD_POWER_POWER_STATUS_STARTUP) {
-		goto try_re_enable
-	} else if (a == JD_POWER_POWER_STATUS_POWERING) {
+	if (a == JD_POWER_POWER_STATUS_POWERING) {
 		set1 txp_pwr_shutdown
+	} else if (a == JD_POWER_POWER_STATUS_OVERLOAD) {
+		set1 txp_pwr_shutdown
+	} else if (a == JD_POWER_POWER_STATUS_OVERPROVISION) {
+		dzsn switch_cnt
+			goto loop
+		goto try_re_enable
 	}
+
 	goto loop
 
 try_re_enable:
@@ -98,7 +99,9 @@ try_re_enable:
 	mov a, pwr_status
 	ifset ZF	// if Disallowed, don't re-enable
 		goto loop
-	.mova pwr_status, JD_POWER_POWER_STATUS_POWERING
+	pwr_status = JD_POWER_POWER_STATUS_POWERING
+	// shedule shutdown to be sent ASAP
+	t_next_shutdown = t16_16ms
 	goto loop
 ENDM
 
@@ -186,11 +189,15 @@ serv_rx:
 		mov a, pkt_service_command_l
 		if (a == JD_POWER_CMD_SHUTDOWN) {
 			mov a, pwr_status
-			if (!ZF) { // don't change status if Disallowed
-				mov a, JD_POWER_POWER_STATUS_OVERPROVISION
-				// if not in grace period, disable limiter
-				ifclear txp_pwr_shutdown
-					mov pwr_status, a
+			sub a, 1
+			ifset ZF
+			    mov a, 1
+			ifset txp_pwr_shutdown
+				mov a, 0
+			if (a == 1) {
+				// (pwr_status == JD_POWER_POWER_STATUS_POWERING || pwr_status == JD_POWER_POWER_STATUS_OVERLOAD) && !txp_pwr_shutdown
+				pwr_status = JD_POWER_POWER_STATUS_OVERPROVISION
+				switch_cnt = 20 // how many "shutdown" cycles before we try to take over powering
 			}
 			.t16_set t16_262ms, t_re_enable, 5 // can re-enable after ~1300ms
 		}
