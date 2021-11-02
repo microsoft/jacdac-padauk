8e912f6c0210eb98e7846436886ebabd5fa85ab6
Riven <4771215@qq.com>
2021-11-02 15:23:13 +0800

    pub event on toggle

--- services/relay.asm
+++ services/relay.asm
@@ -12,26 +12,35 @@
 txp_closed equ txp_serv0
 
     BYTE  closed
+    BYTE  prev_closed
 
 .serv_init EXPAND
     PAC.PIN_RELAY_DRIVE =   1 // output
     PAC.PIN_RELAY_LED   =   1
     PA.PIN_RELAY_DRIVE  =   0
+    prev_closed = 0
     closed = 0
 ENDM
 
 .serv_process EXPAND
+    mov a, closed
+    if (a != prev_closed){
+        mov prev_closed, a
+        goto ev_send
+    }
     .ev_process
 ENDM
 
 .serv_prep_tx EXPAND
     if (txp_closed){
         set0 txp_closed
-		.setcmd JD_HIGH_REG_RW_GET, JD_RELAY_REG_CLOSED
-		.mova pkt_payload[0], closed
-		.mova pkt_size, 1
-		ret
+        .setcmd JD_HIGH_REG_RW_GET, JD_RELAY_REG_CLOSED
+        .mova pkt_payload[0], closed
+        .mova pkt_size, 1
+        ret
     }
+    ifset txp_event
+        goto ev_prep_tx
 ENDM
 
 serv_rx:
@@ -60,8 +69,11 @@ serv_rx:
     goto rx_process_end
 
 .serv_ev_payload EXPAND
-
-
+    if (closed){
+        .mova pkt_service_command_l, JD_EV_ACTIVE
+    } else {
+        .mova pkt_service_command_l, JD_EV_INACTIVE
+    }
 ENDM
 
     .ev_impl
